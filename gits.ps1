753fc967f060d39bafeab82827cbbb104a6e360e
Steve <sjpearce@gmail.com>
2021-05-24 09:27:02 +0100

    Initial commit

---
+++ gits.ps1
@@ -0,0 +1,8 @@
+# add the gitignore file tailored for dotnet applications, this will ignore bin/obj and many other non-source code files
+dotnet new gitignore
+# create the git repository
+git init
+# track all files that are not ignore by .gitignore
+git add --all
+# commit all changes to the repository
+git commit -m "Initial commit"
