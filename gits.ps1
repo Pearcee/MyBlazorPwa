60bdd76aecde33e1866702f72cb1f47df0e42279
Steve <sjpearce@gmail.com>
2021-05-24 09:48:24 +0100

    Initial commit

--- gits.ps1
+++ gits.ps1
@@ -6,3 +6,7 @@ git init
 git add --all
 # commit all changes to the repository
 git commit -m "Initial commit"
+
+git remote add origin https://github.com/Pearcee/MyBlazorPwa.git
+git push -u origin main
+
